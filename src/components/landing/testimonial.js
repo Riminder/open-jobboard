d129731b7eb076d55a6d6ba0e24d94448a1f53b7
Mohammed Nouayti <med.nouayti@gmail.com>
2020-08-18 19:27:12 +0100

    add testimonials and videos sections

---
+++ src/components/landing/testimonial.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import testimonialStyles from './testimonial.module.scss'
+import mock600x700 from '../../assets/images/mock600x700.png'
+
+const Testimonial = props => {
+  return (
+    <section
+      className={testimonialStyles.section}
+      style={{
+        background: props.reversed ? '#f2f2f2' : '',
+        flexDirection: props.reversed ? 'row-reverse' : ''
+      }}
+    >
+        <div className={testimonialStyles.content}>
+          <div className={`${testimonialStyles.left} ${props.reversed ? '' : 'text-white'}`}>
+            <div className="text-large mb-1">
+              Proident consectetur occaecat nulla irure veniam ad ex irure in nisi ad.
+            </div>
+            <div className="text-medium mb-1">
+              Occaecat laborum adipisicing enim incididunt sunt velit minim adipisicing laboris labore consequat. 
+              Ullamco ad magna aliquip magna id amet culpa exercitation Lorem anim adipisicing anim reprehenderit. 
+              Quis exercitation in dolor esse amet.
+            </div>
+            <div className="text-small mb-1">
+              Cillum quis elit id sint tempor proident occaecat et et sunt.
+            </div>
+          </div>
+          <div>
+            <img className={testimonialStyles.img} src={mock600x700} />
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonial
---
+++ src/components/landing/videos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import videosStyles from './videos.module.scss'
+import mock350x450 from '../../assets/images/mock350x450.png'
+
+const Videos = () => {
+  return (
+    <section className={videosStyles.section}>
+      <div className="text-center text-bold-700 text-large">Dolor sint exercitation commodo magna</div>
+      <div className={videosStyles.content}>
+        <div className={videosStyles.img}>
+          <img src={mock350x450} className={`${videosStyles.img} mb-1`}/>
+          <div className="text-medium text-bold-700 text-center">Adipisicing cupidata</div>
+        </div>
+        <div className={videosStyles.img}>
+          <img src={mock350x450} className={`${videosStyles.img} mb-1`}/>
+          <div className="text-medium text-bold-700 text-center">Adipisicing cupidata</div>
+        </div>
+        <div className={videosStyles.img}>
+          <img src={mock350x450} className={`${videosStyles.img} mb-1`}/>
+          <div className="text-medium text-bold-700 text-center">Adipisicing cupidata</div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Videos
