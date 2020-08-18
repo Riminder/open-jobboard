0a18f28f323d323ea299355eb7cc4adc9dfaa914
Mohammed Nouayti <med.nouayti@gmail.com>
2020-08-18 19:26:05 +0100

    add header and header module styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'gatsby'
 import headerStyles from './header.module.scss'
 import logo from "../assets/images/logo.svg"
 
-const Header = () => {
+const Header = props => {
   return (
-      <header>
+
+      <header className={headerStyles.header}>
         <nav className={headerStyles.navbar}>
-          <img src={logo} alt="Logo" />
+          <Link to={"/"}>
+            <img src={logo} alt="Logo" />
+          </Link>
         </nav>
-        <div className={headerStyles.jambotron}>
-          <h1></h1>
-        </div>
+        {props.jambotron && (
+          <div className={headerStyles.jambotron}>
+            <div class={headerStyles.jambotron__heading}>
+              <h1 class="text-extra-large text-white">Fugiat excepteur ut dolore <br></br>esse labore mollit.</h1>
+              <span class="text-medium text-white">Consectetur et officia ut Lorem elit.</span>
+            </div>
+            <div class={headerStyles.jambotron__img}>
+            </div>
+          </div>
+        )}
       </header>
   )
 }
