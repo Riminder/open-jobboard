3d7b6c7ed9e8733de85dd1bd6556b7a0e1bd6f19
Mohammed Nouayti <med.nouayti@gmail.com>
2020-08-18 19:25:38 +0100

    add footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import footerStyles from './footer.module.scss';
+import linkedin from '../assets/images/linkedin.svg';
+import twitter from '../assets/images/twitter.svg';
+import facebook from '../assets/images/facebook.svg';
+import youtube from '../assets/images/youtube.svg';
+import instagram from '../assets/images/instagram.svg';
+import mock146x53 from '../assets/images/mock146x53.png';
+
+
 // import { Link } from 'gatsby';
 
 const Footer = () => {
   return (
-    <footer className={footerStyles.container}>
-      <p>Job board footer</p>
-    </footer>
+    <footer class={footerStyles.section}>
+      <div class={footerStyles.footer_top}>
+        <img src={mock146x53} />
+        <span class="text-medium text-bold-700 text-center">Ullamco eu dolor irure labore.</span>
+      </div>
+      <div class={footerStyles.divider}></div>
+      <div class={footerStyles.footer_bot}>
+          <div class={footerStyles.links}>
+            <a href="#">link</a>
+            <a href="#">link</a>
+            <a href="#">link</a>
+            <a href="#">link</a>
+          </div>
+          <div class={footerStyles.socials}>
+            <a href="#" target="blank"><img src={linkedin} /></a>
+            <a href="#" target="blank"><img src={facebook} /></a>
+            <a href="#" target="blank"><img src={twitter} /></a>
+            <a href="#" target="blank"><img src={youtube} /></a>
+            <a href="#" target="blank"><img src={instagram} /></a>
+        </div>
+      </div>
+  </footer>
   )
 }
 
