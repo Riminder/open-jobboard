4fd63d2842cbf359cfdf02b4afe2a10d2f928193
Mohammed Nouayti <med.nouayti@gmail.com>
2020-12-08 16:03:37 +0100

    job listing

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://api.rimstaging.net/v1',
     headers: { 
-        'X-API-KEY': 'ask_0dec90609f229d31d0bdd6a03da4f588'
+        'X-API-KEY': 'ask_0dec90609f2ksk45454sd44545dd4'
     }
 });
 
