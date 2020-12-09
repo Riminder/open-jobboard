9822266f9fc16698cc06803484e0dad31f6b4b61
Mohammed Nouayti <med.nouayti@gmail.com>
2020-12-09 23:45:38 +0100

    add api key in config file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { API_KEY } from './config'
 
 const instance = axios.create({
     baseURL: 'https://api.rimstaging.net/v1',
     headers: { 
-        'X-API-KEY': 'ask_0dec90609f2ksk45454sd44545dd4'
+        'X-API-KEY': API_KEY,
     }
 });
 
