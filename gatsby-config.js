6664fa939057dc2dd5f88dfe5f33ce5077189623
Mohammed Nouayti <med.nouayti@gmail.com>
2020-12-10 17:15:43 +0100

    add gatsby seo config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,28 @@
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Your job board title',
+    description: `
+      Your job board description
+    `,
+    siteUrl: 'https://example.com',
+    image: 'your og image url',
+    author:  'HrFlow.ai',
+    organization: {
+      name: 'Hrflow.ai',
+      url: 'https://hrflow.ai',
+      logo: 'https://img.riminder.net/logo/Logo.svg',
+    },
+  },
   plugins: [
     'gatsby-plugin-sass',
     '@bumped-inc/gatsby-plugin-optional-chaining',
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-root-import',
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "your s3 bucket name",
+      },
+    },
   ],
 }
